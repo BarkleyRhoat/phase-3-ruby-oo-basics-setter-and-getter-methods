aa8b7ab07f54ee8d00b220747db4c20eaf8ecd1d
BarkleyRhoat <barkleyrhoat@gmail.com>
2026-07-07 15:23:47 -0400

    oo basics setter and getter

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,12 @@
 class Dog
+  attr_accessor :breed
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
 end
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,2 +1,12 @@
 class Person
+  attr_accessor :name, :job
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
 end
+
